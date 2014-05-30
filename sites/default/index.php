37c5f81c904efd9249c6b802bf926262d02e1fe9
Chris Wiegman <cwiegman@me.com>
2014-05-30 16:27:26 -0500

    Add default index

---
+++ sites/default/index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<title>Primary Vagrant</title>
+</head>
+<body>
+
+	<h1>Welcome to Primary Vagrant</h1>
+
+<h2>About Primary Vagrant</h2>
+
+	<p>Primary Vagrant is an Apache Based Development Environment for working with WordPress on your local machine.</p>
+
+	<ul>
+		<li><a href="https://github.com/ChrisWiegman/Primary-Vagrant" target="_blank">View the Primary Vagrant Project on GitHub</a></li>
+	</ul>
+
+<h2>Primary Vagrant Tools</h2>
+
+	<ul>
+		<li><a href="http://phpmyadmin.vagrant" target="_blank">Manage MySQL with phpMyAdmin</a></li>
+		<li><a href="http://replacedb.vagrant" target="_blank">Easily Search and Replace MySQL database values with Search and Replace DB</a>< (this makes migrating sites a breeze)/li>
+		<li><a href="http://webgrind.vagrant" target="_blank">Profile your application with Webgrind</a></li>
+	</ul>
+
+<h2>Development Sites</h2>
+
+	<ul>
+		<li><a href="http://stable.wordpress.vagrant" target="_blank">WordPress Stable Branch (current version)</a></li>
+		<li><a href="http://trunk.wordpress.vagrant" target="_blank">WordPress Trunk (development) Version</a></li>
+	</ul>
+
+</body>
+</html>
