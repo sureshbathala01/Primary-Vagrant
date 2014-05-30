ed25df500b70cd4eb5ce93ded371951d640f0263
Chris Wiegman <cwiegman@me.com>
2014-05-30 15:08:04 -0500

    Fix xdebug config

--- manifests/init.pp
+++ manifests/init.pp
@@ -82,81 +82,81 @@ php::pecl::module { "xdebug": }
 
 php::augeas {
   'php-xdebug.trace_enable_trigger':
-  entry  => 'XDEBUG/xdebug.trace_enable_trigger',
-  value  => '1',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.trace_enable_trigger',
+    value  => '1',
+    require => Class['php'];
   'xdebug.trace_output_dir':
-  entry  => 'XDEBUG/xdebug.trace_output_dir',
-  value  => '/var/xdebug',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.trace_output_dir',
+    value  => '/var/xdebug',
+    require => Class['php'];
   'php-xdebug.collect_includes':
-  entry  => 'XDEBUG/xdebug.collect_includes',
-  value  => '1',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.collect_includes',
+    value  => '1',
+    require => Class['php'];
   'php-xdebug.collect_params':
-  entry  => 'XDEBUG/xdebug.collect_params',
-  value  => '1',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.collect_params',
+    value  => '1',
+    require => Class['php'];
   'php-xdebug.collect_vars':
-  entry  => 'XDEBUG/xdebug.collect_vars',
-  value  => '1',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.collect_vars',
+    value  => '1',
+    require => Class['php'];
   'php-xdebug.collect_return':
-  entry  => 'XDEBUG/xdebug.collect_return',
-  value  => '1',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.collect_return',
+    value  => '1',
+    require => Class['php'];
   'php-xdebug.dump_globals':
-  entry  => 'XDEBUG/xdebug.dump_globals',
-  value  => '1',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.dump_globals',
+    value  => '1',
+    require => Class['php'];
   'php-xdebug.idekey':
-  entry  => 'XDEBUG/xdebug.idekey',
-  value  => '"VAGRANT_DEBUG"',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.idekey',
+    value  => 'VAGRANT_DEBUG',
+    require => Class['php'];
   'php-xdebug.profiler_enable_trigger':
-  entry  => 'XDEBUG/xdebug.profiler_enable_trigger',
-  value  => '1',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.profiler_enable_trigger',
+    value  => '1',
+    require => Class['php'];
   'php-xdebug.profiler_output_name':
-  entry  => 'XDEBUG/xdebug.profiler_output_name',
-  value  => '"cachegrind.out.%t-%s"',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.profiler_output_name',
+    value  => 'cachegrind.out.%t-%s',
+    require => Class['php'];
   'php-xdebug.profiler_output_dir':
-  entry  => 'XDEBUG/xdebug.profiler_output_dir',
-  value  => '"/var/xdebug"',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.profiler_output_dir',
+    value  => '/var/xdebug',
+    require => Class['php'];
   'php-xdebug.remote_enable':
-  entry  => 'XDEBUG/xdebug.remote_enable',
-  value  => '1',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.remote_enable',
+    value  => '1',
+    require => Class['php'];
   'php-xdebug.remote_mode':
-  entry  => 'XDEBUG/xdebug.remote_mode',
-  value  => 'jit',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.remote_mode',
+    value  => 'jit',
+    require => Class['php'];
   'php-xdebug.remote_host':
-  entry  => 'XDEBUG/xdebug.remote_host',
-  value  => '"192.168.56.1"',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.remote_host',
+    value  => '192.168.56.1',
+    require => Class['php'];
   'php-xdebug.remote_log':
-  entry  => 'XDEBUG/xdebug.remote_log',
-  value  => '/var/xdebug/xdebug-remote.log',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.remote_log',
+    value  => '/var/xdebug/xdebug-remote.log',
+    require => Class['php'];
   'php-xdebug.remote_port':
-  entry  => 'XDEBUG/xdebug.remote_port',
-  value  => '9000',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.remote_port',
+    value  => '9000',
+    require => Class['php'];
   'php-xdebug.var_display_max_children':
-  entry  => 'XDEBUG/xdebug.var_display_max_children',
-  value  => '-1',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.var_display_max_children',
+    value  => '-1',
+    require => Class['php'];
   'php-xdebug.var_display_max_data':
-  entry  => 'XDEBUG/xdebug.var_display_max_data',
-  value  => '-1',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.var_display_max_data',
+    value  => '-1',
+    require => Class['php'];
   'php-xdebug.var_display_max_depth':
-  entry  => 'XDEBUG/xdebug.var_display_max_depth',
-  value  => '-1',
-  require => Class['php'];
+    entry  => 'XDEBUG/xdebug.var_display_max_depth',
+    value  => '-1',
+    require => Class['php'];
 }
 
 class { 'wp-cli': }
