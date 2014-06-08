cba82bcbd50c1f64e6c1562a075c0846cff6fb14
Chris Wiegman <cwiegman@me.com>
2014-06-08 11:12:00 -0500

    Cleanup code formatting

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,19 @@
 group { 'puppet': ensure => present }
+
 Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ] }
 File { owner => 0, group => 0, mode => 0644 }
 
-class {'apt':
+class { 'apt':
 	always_apt_update => true,
 }
 
 Class['::apt::update'] -> Package <|
-title != 'python-software-properties'
-and title != 'software-properties-common'
+	title != 'python-software-properties'
+	and title != 'software-properties-common'
 |>
 
-package { "vim":
-	ensure => "installed"
+package { 'vim':
+	ensure => 'installed'
 }
 
 class { 'ohmyzsh': }
@@ -23,54 +24,54 @@ class { 'apache': }
 include apache::ssl
 
 apache::vhost { 'pv':
-	docroot                    => '/var/www/pv',
+	docroot => '/var/www/pv',
 }
 
 apache::vhost { 'phpmyadmin.pv':
-	docroot                    => '/var/www/phpmyadmin.pv',
-	directory                    => '/var/www/phpmyadmin.pv',
-	directory_allow_override    => 'All',
-	ssl                            => true,
-	template                    => '/var/vagrant/conf/vhost.conf.erb',
+	docroot                  => '/var/www/phpmyadmin.pv',
+	directory                => '/var/www/phpmyadmin.pv',
+	directory_allow_override => 'All',
+	ssl                      => true,
+	template                 => '/var/vagrant/conf/vhost.conf.erb',
 }
 
 apache::vhost { 'replacedb.pv':
-	docroot                    => '/var/www/replacedb.pv',
-	directory                    => '/var/www/replacedb.pv',
-	directory_allow_override    => 'All',
-	template                    => '/var/vagrant/conf/vhost.conf.erb',
+	docroot                  => '/var/www/replacedb.pv',
+	directory                => '/var/www/replacedb.pv',
+	directory_allow_override => 'All',
+	template                 => '/var/vagrant/conf/vhost.conf.erb',
 }
 
 apache::vhost { 'wordpress.core.pv':
-	docroot                    => '/var/www/wordpress.core.pv/src',
-	directory                    => '/var/www/wordpress.core.pv/src',
-	directory_allow_override    => 'All',
-	ssl                            => true,
-	template                    => '/var/vagrant/conf/vhost.conf.erb',
+	docroot                  => '/var/www/wordpress.core.pv/src',
+	directory                => '/var/www/wordpress.core.pv/src',
+	directory_allow_override => 'All',
+	ssl                      => true,
+	template                 => '/var/vagrant/conf/vhost.conf.erb',
 }
 
 apache::vhost { 'wordpress.stable.pv':
-	docroot                    => '/var/www/wordpress.stable.pv/wordpress',
-	directory                    => '/var/www/wordpress.stable.pv/wordpress',
-	directory_allow_override    => 'All',
-	ssl                            => true,
-	template                    => '/var/vagrant/conf/vhost.conf.erb',
+	docroot                  => '/var/www/wordpress.stable.pv/wordpress',
+	directory                => '/var/www/wordpress.stable.pv/wordpress',
+	directory_allow_override => 'All',
+	ssl                      => true,
+	template                 => '/var/vagrant/conf/vhost.conf.erb',
 }
 
 apache::vhost { 'wordpress.trunk.pv':
-	docroot                    => '/var/www/wordpress.trunk.pv/wordpress',
-	directory                    => '/var/www/wordpress.trunk.pv/wordpress',
-	directory_allow_override    => 'All',
-	ssl                            => true,
-	template                    => '/var/vagrant/conf/vhost.conf.erb',
+	docroot                  => '/var/www/wordpress.trunk.pv/wordpress',
+	directory                => '/var/www/wordpress.trunk.pv/wordpress',
+	directory_allow_override => 'All',
+	ssl                      => true,
+	template                 => '/var/vagrant/conf/vhost.conf.erb',
 }
 
 apache::vhost { 'webgrind.pv':
-	docroot                    => '/var/www/webgrind.pv',
-	directory                    => '/var/www/webgrind.pv',
-	directory_allow_override    => 'All',
-	ssl                            => true,
-	template                    => '/var/vagrant/conf/vhost.conf.erb',
+	docroot                  => '/var/www/webgrind.pv',
+	directory                => '/var/www/webgrind.pv',
+	directory_allow_override => 'All',
+	ssl                      => true,
+	template                 => '/var/vagrant/conf/vhost.conf.erb',
 }
 
 class { 'php':
@@ -93,92 +94,92 @@ php::module { 'php5-curl': }
 php::module { 'php5-gd': }
 php::module { 'php5-imagick': }
 php::module { 'php-apc': }
-php::pecl::module { "xdebug": }
+php::pecl::module { 'xdebug': }
 
 php::augeas {
 	'php-xdebug.trace_enable_trigger':
-	entry  => 'XDEBUG/xdebug.trace_enable_trigger',
-	value  => '1',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.trace_enable_trigger',
+		value   => '1',
+		require => Class['php'];
 	'xdebug.trace_output_dir':
-	entry  => 'XDEBUG/xdebug.trace_output_dir',
-	value  => '/var/xdebug',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.trace_output_dir',
+		value   => '/var/xdebug',
+		require => Class['php'];
 	'php-xdebug.collect_includes':
-	entry  => 'XDEBUG/xdebug.collect_includes',
-	value  => '1',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.collect_includes',
+		value   => '1',
+		require => Class['php'];
 	'php-xdebug.collect_params':
-	entry  => 'XDEBUG/xdebug.collect_params',
-	value  => '1',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.collect_params',
+		value   => '1',
+		require => Class['php'];
 	'php-xdebug.collect_vars':
-	entry  => 'XDEBUG/xdebug.collect_vars',
-	value  => '1',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.collect_vars',
+		value   => '1',
+		require => Class['php'];
 	'php-xdebug.collect_return':
-	entry  => 'XDEBUG/xdebug.collect_return',
-	value  => '1',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.collect_return',
+		value   => '1',
+		require => Class['php'];
 	'php-xdebug.dump_globals':
-	entry  => 'XDEBUG/xdebug.dump_globals',
-	value  => '1',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.dump_globals',
+		value   => '1',
+		require => Class['php'];
 	'php-xdebug.idekey':
-	entry  => 'XDEBUG/xdebug.idekey',
-	value  => 'VAGRANT_DEBUG',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.idekey',
+		value   => 'VAGRANT_DEBUG',
+		require => Class['php'];
 	'php-xdebug.profiler_enable_trigger':
-	entry  => 'XDEBUG/xdebug.profiler_enable_trigger',
-	value  => '1',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.profiler_enable_trigger',
+		value   => '1',
+		require => Class['php'];
 	'php-xdebug.profiler_output_name':
-	entry  => 'XDEBUG/xdebug.profiler_output_name',
-	value  => 'cachegrind.out.%t-%s',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.profiler_output_name',
+		value   => 'cachegrind.out.%t-%s',
+		require => Class['php'];
 	'php-xdebug.profiler_output_dir':
-	entry  => 'XDEBUG/xdebug.profiler_output_dir',
-	value  => '/var/xdebug',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.profiler_output_dir',
+		value   => '/var/xdebug',
+		require => Class['php'];
 	'php-xdebug.remote_enable':
-	entry  => 'XDEBUG/xdebug.remote_enable',
-	value  => '1',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.remote_enable',
+		value   => '1',
+		require => Class['php'];
 	'php-xdebug.remote_mode':
-	entry  => 'XDEBUG/xdebug.remote_mode',
-	value  => 'req',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.remote_mode',
+		value   => 'req',
+		require => Class['php'];
 	'php-xdebug.remote_host':
-	entry  => 'XDEBUG/xdebug.remote_host',
-	value  => '192.168.13.1',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.remote_host',
+		value   => '192.168.13.1',
+		require => Class['php'];
 	'php-xdebug.remote_log':
-	entry  => 'XDEBUG/xdebug.remote_log',
-	value  => '/var/xdebug/xdebug-remote.log',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.remote_log',
+		value   => '/var/xdebug/xdebug-remote.log',
+		require => Class['php'];
 	'php-xdebug.remote_port':
-	entry  => 'XDEBUG/xdebug.remote_port',
-	value  => '9000',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.remote_port',
+		value   => '9000',
+		require => Class['php'];
 	'php-xdebug.var_display_max_children':
-	entry  => 'XDEBUG/xdebug.var_display_max_children',
-	value  => '-1',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.var_display_max_children',
+		value   => '-1',
+		require => Class['php'];
 	'php-xdebug.var_display_max_data':
-	entry  => 'XDEBUG/xdebug.var_display_max_data',
-	value  => '-1',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.var_display_max_data',
+		value   => '-1',
+		require => Class['php'];
 	'php-xdebug.var_display_max_depth':
-	entry  => 'XDEBUG/xdebug.var_display_max_depth',
-	value  => '-1',
-	require => Class['php'];
+		entry   => 'XDEBUG/xdebug.var_display_max_depth',
+		value   => '-1',
+		require => Class['php'];
 }
 
 class { 'wp-cli': }
 
-class {'postfix':
-	relayhost       => '127.0.0.1',
-	relayhost_port  => '1025',
+class { 'postfix':
+	relayhost      => '127.0.0.1',
+	relayhost_port => '1025',
 }
 
 class { 'mysql::server':
@@ -204,8 +205,8 @@ mysql_database { 'wordpress.core.pv':
 }
 
 mysql_user { 'username@localhost':
-	ensure  => 'present',
-	require => Class['mysql::server'],
+	ensure        => 'present',
+	require       => Class['mysql::server'],
 	password_hash => mysql_password(password),
 }
 
