0e87c9a0115ae0e0c746d36ab9108d4047d44abe
Chris Wiegman <cwiegman@me.com>
2014-07-18 10:09:40 -0500

    Update configs

--- sites/wordpress/trunk/htdocs/index.php
+++ sites/wordpress/trunk/htdocs/index.php
@@ -1,3 +1,3 @@
 <?php
-define( 'WP_USE_THEMES', true );
+define( 'WP_USE_THEMES', false );
 require( './wp/wp-blog-header.php' );
--- sites/wordpress/trunk/htdocs/wp-config.php
+++ sites/wordpress/trunk/htdocs/wp-config.php
@@ -97,4 +97,6 @@ if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+if ( !defined( 'ABSPATH' ) )
+	define( 'ABSPATH', dirname( __FILE__ ) . '/wordpress/' );
+require_once( ABSPATH . 'wp-settings.php' );
