6982380cb54cfb316bddb57cd43974956fd11930
Chris Wiegman <cwiegman@me.com>
2014-07-18 10:36:59 -0500

    Update links in default website

--- sites/default/index.php
+++ sites/default/index.php
@@ -27,9 +27,9 @@
 
 	<ul>
 		<li><a href="http://wordpress.core.pv" target="_blank">WordPress Core (src folder for WordPress core development)</a></li>
-		<li><a href="http://wordpress.legacy.pv" target="_blank">WordPress Legacy Branch (old version - 3.8.3)</a></li>
-		<li><a href="http://wordpress.stable.pv" target="_blank">WordPress Stable Branch (current version - 3.9.1)</a></li>
-		<li><a href="http://wordpress.trunk.pv" target="_blank">WordPress Trunk (development) Version</a></li>
+		<li><a href="http://wordpress.legacy.pv/wordpress" target="_blank">WordPress Legacy Branch (old version - 3.8.3)</a></li>
+		<li><a href="http://wordpress.stable.pv/wordpress" target="_blank">WordPress Stable Branch (current version - 3.9.1)</a></li>
+		<li><a href="http://wordpress.trunk.pv/wordpress" target="_blank">WordPress Trunk (development) Version</a></li>
 	</ul>
 
 </body>
