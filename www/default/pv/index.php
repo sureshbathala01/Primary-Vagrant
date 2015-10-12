b1c50a1ae2c46134f590401500d03d5caddb1a1d
Chris Wiegman <chris@chriswiegman.com>
2015-10-12 09:45:36 -0400

    Fix typo in tools list

--- www/default/pv/index.php
+++ www/default/pv/index.php
@@ -18,7 +18,7 @@
 
 	<ul>
 		<li><a href="http://phpmyadmin.pv" target="_blank">Manage MySQL with phpMyAdmin</a></li>
-		<li><a href="http://replacedb.pv" target="_blank">Easily Search and Replace MySQL database values with Search and Replace DB</a>< (this makes migrating sites a breeze)/li>
+		<li><a href="http://replacedb.pv" target="_blank">Easily Search and Replace MySQL database values with Search and Replace DB</a> (this makes migrating sites a breeze)</li>
 		<li><a href="http://webgrind.pv" target="_blank">Profile your application with Webgrind</a></li>
 		<li><a href="http://mailcatcher.pv:1080" target="_blank">Collect email with MailCatcher</a></li>
 	</ul>
