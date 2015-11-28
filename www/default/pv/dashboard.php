5b208a51313a6a6791dd3e0698a66417e981ad02
crazyjaco <crazyjac0@gmail.com>
2015-11-28 12:46:14 -0500

    Added a very simple mechanism to check for a custom dashboard. Dashboards can be added to a folder named custom within the PV site and adding an index.php file.

---
+++ www/default/pv/dashboard.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+	<title>Primary Vagrant</title>
+</head>
+<body>
+
+	<h1>Welcome to Primary Vagrant</h1>
+
+<h2>About Primary Vagrant</h2>
+
+	<p>Primary Vagrant is an Apache Based Development Environment for working with WordPress on your local machine.</p>
+
+	<ul>
+		<li><a href="https://github.com/ChrisWiegman/Primary-Vagrant" target="_blank">View the Primary Vagrant Project on GitHub</a></li>
+	</ul>
+
+<h2>Primary Vagrant Tools</h2>
+
+	<ul>
+		<li><a href="http://phpmyadmin.pv" target="_blank">Manage MySQL with phpMyAdmin</a></li>
+		<li><a href="http://replacedb.pv" target="_blank">Easily Search and Replace MySQL database values with Search and Replace DB</a> (this makes migrating sites a breeze)</li>
+		<li><a href="http://webgrind.pv" target="_blank">Profile your application with Webgrind</a></li>
+		<li><a href="http://mailcatcher.pv:1080" target="_blank">Collect email with MailCatcher</a></li>
+	</ul>
+
+<h2>Development Sites</h2>
+
+	<ul>
+		<li><a href="http://core.wordpress.pv" target="_blank">WordPress Core (src folder for WordPress core development)</a></li>
+		<li><a href="http://legacy.wordpress.pv" target="_blank">WordPress Legacy Branch (old version - 4.2.x)</a></li>
+		<li><a href="http://stable.wordpress.pv" target="_blank">WordPress Stable Branch (current version - 4.3.x)</a></li>
+		<li><a href="http://trunk.wordpress.pv" target="_blank">WordPress Trunk (development) Version</a></li>
+	</ul>
+
+</body>
+</html>
