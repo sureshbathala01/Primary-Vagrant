8c69f87a3b8db0564b70f4eb899dfe401671a3f4
Chris Wiegman <chris@chriswiegman.com>
2016-04-20 12:41:27 -0400

    add line helper from VIP Quickstart

---
+++ manifests/helpers/line.pp
@@ -0,0 +1,17 @@
+# line
+# Taken from Automattic's VIP Quickstart (https://github.com/Automattic/vip-quickstart)
+define line($file, $line, $ensure = 'present') {
+  case $ensure {
+    default : { err ( "unknown ensure value ${ensure}" ) }
+    present: {
+      exec { "/bin/echo '${line}' >> '${file}'":
+        unless => "/bin/grep -qFx '${line}' '${file}'"
+      }
+    }
+    absent: {
+      exec { "/usr/bin/perl -ni -e 'print unless /^\\Q${line}\\E\$/' '${file}'":
+        onlyif => "/bin/grep -qFx '${line}' '${file}'"
+      }
+    }
+  }
+}
