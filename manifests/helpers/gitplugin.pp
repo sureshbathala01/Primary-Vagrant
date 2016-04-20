6d5f4ffff0154f250c1cf478854703b1cd8549d8
Chris Wiegman <chris@chriswiegman.com>
2016-04-20 12:38:14 -0400

    Cleanup gitplugin helper

---
+++ manifests/helpers/gitplugin.pp
@@ -0,0 +1,20 @@
+# GitPlugin clones a git repo into the www/wp-content/plugins directory and activates it in WordPress
+# Taken from Automattic's VIP Quickstart (https://github.com/Automattic/vip-quickstart)
+define gitplugin ( $git_urls ) {
+  vcsrepo { "/srv/www/wp-content/plugins/${title}" :
+    ensure   => present,
+    force    => true,
+    source   => $git_urls[$title],
+    provider => git,
+    require  => [
+      Wp::Site['/srv/www/wp'],
+      File['/srv/www/wp-content/plugins'],
+    ]
+  }
+
+  wp::command { "plugin activate ${title}":
+    command  => "plugin activate ${title} --network",
+    location => '/srv/www/wp',
+    require  => Vcsrepo["/srv/www/wp-content/plugins/${title}"],
+  }
+}
