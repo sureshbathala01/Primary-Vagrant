742c8aa108c2c501f7faca4b109346b4f35880e0
Chris Wiegman <chris@chriswiegman.com>
2016-04-20 12:38:51 -0400

    Add initial VIP configuration

--- manifests/init.pp
+++ manifests/init.pp
@@ -369,10 +369,109 @@ vcsrepo { '/var/www/replacedb.pv':
   source   => 'https://github.com/interconnectit/Search-Replace-DB.git',
 }
 
+vcsrepo { '/var/www/vip.wordpress.pv/wp':
+  ensure   => present,
+  provider => svn,
+  source   => 'http://core.svn.wordpress.org/trunk/',
+}
+
+vcsrepo { '/var/www/vip.wordpress.pv/wp-tests':
+  ensure   => present,
+  provider => svn,
+  source   => 'http://develop.svn.wordpress.org/trunk/',
+}
+
 vcsrepo { '/var/www/trunk.wordpress.pv/htdocs/content/wp-test':
   ensure   => latest,
   provider => git,
   source   => 'https://github.com/manovotny/wptest.git',
+}
+
+$plugins = [
+  'log-deprecated-notices',
+  'monster-widget',
+  'query-monitor',
+  'user-switching',
+  'wordpress-importer',
+
+  # WordPress.com
+  'keyring',
+  'mrss',
+  'polldaddy',
+  'rewrite-rules-inspector',
+]
+
+$github_plugins = {
+  'vip-scanner'    => 'https://github.com/Automattic/vip-scanner',
+
+  # WordPress.com
+  'jetpack'        => 'https://github.com/Automattic/jetpack',
+  'media-explorer' => 'https://github.com/Automattic/media-explorer',
+  'writing-helper' => 'https://github.com/automattic/writing-helper',
+  'amp'            => 'https://github.com/automattic/amp-wp',
+}
+
+# Delete broken plugins
+file { '/var/www/vip.wordpress.pv/wp-content/plugins/log-viewer':
+  ensure => 'absent',
+  force  => true,
+  before => Wp::Site['/var/www/vip.wordpress.pv/wp'],
+}
+
+# Install WordPress
+wp::site { '/var/www/vip.wordpress.pv/wp':
+  url             => 'vip.wordpress.pv',
+  sitename        => 'vip.wordpress.pv',
+  admin_user      => 'wordpress',
+  admin_password  => 'wordpress',
+  network         => true,
+  require         => [
+    Vcsrepo['/var/www/vip.wordpress.pv/wp'],
+    Line['path:/var/www/vip.wordpress.pv/wp'],
+  ]
+}
+
+# Install GitHub Plugins
+$github_plugin_keys = keys( $github_plugins )
+gitplugin { $github_plugin_keys:
+  git_urls => $github_plugins
+}
+
+# Install plugins
+wp::plugin { $plugins:
+  location    => '/var/www/vip.wordpress.pv/wp',
+  networkwide => true,
+  require     => [
+    Wp::Site['/var/www/vip.wordpress.pv/wp'],
+    File['/srv/www/wp-content/plugins'],
+    Gitplugin[ $github_plugin_keys ],
+  ]
+}
+
+# Update all the plugins
+wp::command { 'plugin update --all':
+  command  => 'plugin update --all',
+  location => '/var/www/vip.wordpress.pv/wp',
+  require  => Wp::Site['/var/www/vip.wordpress.pv/wp'],
+}
+
+# Symlink db.php for Query Monitor
+file { '/var/www/vip.wordpress.pv/wp-content/db.php':
+  ensure  => 'link',
+  target  => 'plugins/query-monitor/wp-content/db.php',
+  require => Wp::Plugin['query-monitor']
+}
+
+vcsrepo { '/var/www/vip.wordpress.pv/wp-content/themes/vip/plugins':
+  ensure   => latest,
+  source   => 'https://vip-svn.wordpress.com/plugins/',
+  provider => svn,
+}
+
+vcsrepo { '/var/www/vip.wordpress.pv/wp-content/themes/pub/twentyfifteen':
+  ensure   => latest,
+  source   => 'https://wpcom-themes.svn.automattic.com/twentyfifteen',
+  provider => svn,
 }
 
 import 'sites/*.pp'
