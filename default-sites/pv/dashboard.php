2b0ac188dd5cbae73a52ce4123d9672104f1b4a5
Chris Wiegman <chris@chriswiegman.com>
2016-05-23 10:50:43 -0400

    Update dashboard for mailhog

--- default-sites/pv/dashboard.php
+++ default-sites/pv/dashboard.php
@@ -20,7 +20,7 @@
 		<li><a href="http://phpmyadmin.pv" target="_blank">Manage MySQL with phpMyAdmin</a></li>
 		<li><a href="http://replacedb.pv" target="_blank">Easily Search and Replace MySQL database values with Search and Replace DB</a> (this makes migrating sites a breeze)</li>
 		<li><a href="http://webgrind.pv" target="_blank">Profile your application with Webgrind</a></li>
-		<li><a href="http://mailcatcher.pv:1080" target="_blank">Collect email with MailCatcher</a></li>
+		<li><a href="http://pv:8025" target="_blank">Collect email with MailHog</a></li>
 	</ul>
 
 <h2>Development Sites</h2>
